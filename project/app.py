19a5abed2fecbb8192ae15105cf4bbb0d9258051
shaylaing <shaydavidlaing@gmail.com>
2026-01-26 23:01:52 +0000

    Configured Flask and routes

---
+++ project/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect
+
+# Configure Flask application
+app = Flask(__name__)
+
+# Define template routes
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/analyser")
+def analyser():
+    return render_template("analyser.html")
+
+@app.route("/simulator")
+def simulator():
+    return render_template("simulator.html")
