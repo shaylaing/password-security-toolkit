9050387601a6d2c8f0ab4ebfb779ab347b6c00c6
shaylaing <shaydavidlaing@gmail.com>
2026-02-23 01:15:07 +0000

    Minimal formatting.

--- project/app.py
+++ project/app.py
@@ -4,10 +4,12 @@ import analyser
 # Configure Flask application
 app = Flask(__name__)
 
+
 # Define template routes
 @app.route("/")
 def index():
     return render_template("index.html", current_page=request.path)
+
 
 @app.route("/analyser", methods=["GET", "POST"])
 def analyser():
@@ -25,7 +27,7 @@ def analyser():
 
     return render_template("analyser.html", current_page=request.path)
 
+
 @app.route("/simulator", methods=["GET", "POST"])
 def simulator():
     return render_template("simulator.html", current_page=request.path) 
-
