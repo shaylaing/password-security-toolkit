adf4eaa59d4911a2580c8868c592a5da99bf8afb
shaylaing <shaydavidlaing@gmail.com>
2026-03-08 18:23:52 +0000

    Defined assumptions for hardware benchmarks to be used in attack simulation.

--- project/simulator.py
+++ project/simulator.py
@@ -11,8 +11,15 @@ wordset = set(wordlist)
 # https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10k-most-common.txt
 
 
-# Declare assumptions for three levels of computational hardware power the attacker could possess
+# Declare assumptions for three levels of computational hardware power the attacker could possess:
+# Online Attack Scenario - one thousand guesses per second (assumes minimal rate limiting and poor configuration)
+online_benchmark = 10 ** 3
 
+# Offline Attack Scenario - ten billion guesses per second (assumes high-end GPU)
+offline_benchmark = 10 ** 10
+
+# Specialised Attack Scenario - one hundred trillion guesses per second (cracking arrays, botnets, GPU clusters, etc.)
+specialised_benchmark = 10 ** 14
 
 # Sources used to inform hardware assumptions can be found at:
 # https://www.onlinehashcrack.com/guides/password-recovery/bruteforce-attack-limits-calculate-time-needed.php
