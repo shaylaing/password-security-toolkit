c8246f2685f069c918f179798d81f24aa90a448b
shaylaing <shaydavidlaing@gmail.com>
2026-03-31 19:04:34 +0100

    Formatted JavaScript files via Prettier.

--- project/static/analyser.js
+++ project/static/analyser.js
@@ -1,18 +1,20 @@
 // Input validation for password submission:
 // Listen for user submitting their inputted password
-document.getElementById("strengthAnalyser").addEventListener("submit", function(event){
-    // Get password input textbox 
-    const passwordField = document.getElementById("passwordTextbox");
+document
+    .getElementById("strengthAnalyser")
+    .addEventListener("submit", function (event) {
+        // Get password input textbox
+        const passwordField = document.getElementById("passwordTextbox");
 
-    // Prevent inputted password from being longer than 64 characters
-    if (passwordField.value.length > 64) {
-        // Stop submission
-        event.preventDefault()
+        // Prevent inputted password from being longer than 64 characters
+        if (passwordField.value.length > 64) {
+            // Stop submission
+            event.preventDefault();
 
-        // Hide results
-        document.getElementById("results").hidden = true
+            // Hide results
+            document.getElementById("results").hidden = true;
 
-        // Show error message
-        document.getElementById("client-length-error").hidden = false
-    }
-});
+            // Show error message
+            document.getElementById("client-length-error").hidden = false;
+        }
+    });
